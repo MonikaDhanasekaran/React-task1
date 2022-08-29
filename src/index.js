8ee686636952e4fb140e36658fcad887b24fec5c
MonikaDhanasekaran <monishri44@gmail.com>
2022-08-29 17:23:59 +0530

    Finished

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    
+  //Code for Function component
+  <div style={{ paddingLeft: "20px" }}>
+    <App
+      componentName="App Functional Component"
+      componentType="Functional Component"
+      response={{ name: "XXX", age: 20 }}
+    />
+  </div>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+);
+  
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
